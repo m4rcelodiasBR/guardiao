d97dde419ea84e12879e9337f103815a742351ee
Marcelo Dias <m4rcelodias@live.com>
2025-06-11 15:25:09 -0300

    Primeiro commit

---
+++ src/main/java/br/com/guardiao/guardiao/GuardiaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.guardiao.guardiao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardiaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuardiaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/guardiao/guardiao/GuardiaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guardiao.guardiao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuardiaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
