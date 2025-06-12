a97904b7ea502db1bf3f76bc0203f639fd158dd8
Marcelo Dias <m4rcelodias@live.com>
2025-06-11 21:53:54 -0300

    Adicionando Spring Security e confogurando acesso ao sistema pelo navegador. Ainda com erro.

---
+++ src/main/java/br/com/guardiao/guardiao/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package br.com.guardiao.guardiao.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        // AQUI ESTÁ A CORREÇÃO: Adicionamos "/" e "/index.html" à lista
+                        .requestMatchers("/", "/index.html", "/css/**", "/js/**", "/fonts/**", "/api/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
