1305ac685332c80d586873d6dfbb96948d1126b6
Marcelo Dias na CPO <m4rcelodias@live.com>
2025-07-18 13:54:33 -0300

    refactor: aplicação para deploy
    - configurado arquivo application.properties para deploy em servidor

--- src/main/java/br/com/guardiao/guardiao/model/Incumbencia.java
+++ src/main/java/br/com/guardiao/guardiao/model/Incumbencia.java
@@ -2,11 +2,12 @@ package br.com.guardiao.guardiao.model;
 
 public enum Incumbencia {
     INCUMBENCIA_100("100 - Incumbência 1"),
-    INCUMBENCIA_200("200 - Incumbência 2"),
-    INCUMBENCIA_300("300 - Incumbência 3"),
-    INCUMBENCIA_400("400 - Incumbência 4"),
-    INCUMBENCIA_500("500 - Incumbência 5"),
-    INCUMBENCIA_600("600 - Incumbência 6"),
+    INCUMBENCIA_200("200 - Secretário Adjunto"),
+    INCUMBENCIA_212("212 - Subsecretaria de Seleção"),
+    INCUMBENCIA_231("231 - Mestre da CPO"),
+    INCUMBENCIA_232("232 - Paiol de Material"),
+    INCUMBENCIA_233("233 - Departamento de Administração"),
+    INCUMBENCIA_234("234 - Mestre D'Armas"),
     BAIXA("000 - Baixa"),
     DOACAO("001 - Doação"),
     OUTRA_OM("002 - Outra OM");
